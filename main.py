b8b401b192e0774de8f7ee152bf47906ab7d062b
aaroopsie <sigmacute0@gmail.com>
2025-08-30 14:30:26 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# # Introduction to Python and Bisection Method
+
+# print("Hello World!")
+
+# a = 5
+# b = 6
+# print(a+b)
+
+# name="aaro", age="13", price="100000"
+# print(type(name), type(age), type(price))
+
+
+# # Conditional Statement -->
+
+# b=float(input("Enter 2nd number : "))
+
+# a=float(input("Enter any number : "))
+# print(a)
+# if a>0:
+#     print("The number is positive.")
+# elif a<0:
+#     print("The number is negative.")
+# else:
+#     print("The entered number is zero.")
+
+
+# # LOOP -->
+
+for i in range(1, 11, 2):
+    print(i, "Hello World!")
+
+# i = 1
+# while i <= 10:
+#     print(i, "Hello World")
+#     i+=1
